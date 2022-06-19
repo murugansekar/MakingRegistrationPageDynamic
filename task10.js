266d48aba38052b3c9f9efd5f7193bcacdce2588
MURUGAN SEKAR <murugansekar2608@gmail.com>
2022-06-20 00:29:41 +0530

    Add files via upload

---
+++ task10.js
@@ -0,0 +1,56 @@
+const myForm = document.querySelector('#my-form')
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const pnumberInput = document.querySelector('#pnumber')
+const msg = document.querySelector('.msg');
+myForm.addEventListener('submit', onSubmit);
+document.addEventListener('DOMContentLoaded', DisplayApointments)
+
+function DisplayApointments()
+{
+  axios.get("https://crudcrud.com/api/e8adbcd57acc460ebd939c07dfe006d2/appoitments")
+  .then((res) => { let myObj_deserialized = res.data
+  var html = ""
+  for(var i=0;i<myObj_deserialized.length;i++)
+  {
+    html+='<li>' + myObj_deserialized[i].Name +" - " + myObj_deserialized[i].Email +" - " + myObj_deserialized[i].PhoneNumber + ' <button onclick="deleteRow('+i+')"> Delete Apointment </button>' + ' <button onclick="editRow('+i+')"> Edit Apointment </button>' + '</li>'  
+  }
+  document.getElementById("output").innerHTML = html   
+  }).catch((err) => console.log(err))
+}
+
+function onSubmit(e) 
+{
+  e.preventDefault();
+  if(nameInput.value === '' || emailInput.value === '' || pnumberInput.value === '') 
+  {
+    msg.innerHTML = 'Please enter all fields*';
+    msg.style.color = 'red'
+    setTimeout(() => msg.remove(), 5000);
+  } 
+  else 
+  {
+    let myNewObj={Name:nameInput.value,Email:emailInput.value,PhoneNumber:pnumberInput.value}
+    axios.post("https://crudcrud.com/api/e8adbcd57acc460ebd939c07dfe006d2/appoitments",myNewObj).then((res) => {console.log(res.data);DisplayApointments()}).catch((err) => console.log(err))
+    nameInput.value = '';
+    emailInput.value = '';
+    pnumberInput.value = '';   
+  }
+}
+
+function deleteRow(i)
+{
+  let myObj_deserialized = JSON.parse(localStorage.getItem('myObj'))
+  myObj_deserialized.splice(i,1)
+  localStorage.setItem('myObj',JSON.stringify(myObj_deserialized))
+  DisplayApointments();
+}
+
+function editRow(i)
+{
+  let myObj_deserialized = JSON.parse(localStorage.getItem('myObj'))
+  nameInput.value = myObj_deserialized[i].expenseamount;
+  emailInput.value = myObj_deserialized[i].description;
+  pnumberInput.value = myObj_deserialized[i].category;
+  deleteRow(i);
+}
